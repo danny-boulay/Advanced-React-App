3ccb4fa5ffda981f018e3bbe5113266cd90c75e7
danny-boulay <danny.creating@gmail.com>
2025-01-31 10:36:31 -0500

    Test simple pour apprendre à utiliser les tests avec jest et react

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders paragraph with TEST', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText("TEST"); /*Enlever le commentaire correspondant dans App.jspour que le test fonctionne*/ 
+  expect(textElement).toBeInTheDocument();
 });
